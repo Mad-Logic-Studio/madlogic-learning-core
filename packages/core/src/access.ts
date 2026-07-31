43db31499e1f3ee5408b84a54adf8aa868d441fa
Mad Logic Studio <ops@madlogicstudio.com>
2026-07-30 18:53:09 -0600

    feat(access): expose one-click access services (#7)

---
+++ packages/core/src/access.ts
@@ -0,0 +1,6 @@
+import "./access-service-invitations.js";
+import "./access-service-sessions.js";
+
+export * from "./access-contracts.js";
+export * from "./access-security.js";
+export * from "./access-service.js";
